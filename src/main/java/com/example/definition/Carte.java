440d3fd50b317147bce5af94c43d52a0d033de37
Bahir <bahirboudouma2003@gmail.com>
2024-11-15 12:33:22 +0100

    version 3 à corriger

---
+++ src/main/java/com/example/definition/Carte.java
@@ -0,0 +1,38 @@
+package com.example.definition;
+
+public class Carte {
+    int x = 0;
+    int y = 0;
+    char[][] map = {
+        {'H', '.', '.', '.', 'E'},
+        {'.', '.', '.', 'E', '.'},
+        {'.', 'E', '.', '.', '.'},
+        {'.', '.', '.', '.', 'E'},
+        {'E', '.', '.', '.', '.'}
+    };
+    Carte(char [][] map, int x, int y){
+        this.map = map;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void afficherCarte() {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public void deplacerHero(int newX, int newY) {
+        map[x][y] = '.';
+        x = newX;
+        y = newY;
+        if (map[x][y] == 'E') {
+            map[x][y] = 'X';
+        } else {
+            map[x][y] = 'H';
+        }
+    }
+}
+
--- src/main/java/com/example/definition/Gangster.java
+++ src/main/java/com/example/definition/Gangster.java
@@ -2,19 +2,17 @@ package com.example.definition;
 
 public class Gangster extends Personnage{
     int chance = 2;
-    int nbrattaque = 1;
     String arme = "Pistolet";
 
     public Gangster() {
-        super(50, 7, 3, 2, 1, "Pistolet");
+        super(50, 7, 3, 2, "Pistolet");
         this.chance = 2;
         this.arme = "Pistolet";
-        this.nbrattaque = 1;
     }
 
 
     public String toString() {
-        return "Gangster [vie=" + vie + ", attaque=" + attaque + ", defense=" + defense + ", chance=" + chance + ", nbrattaque=" + nbrattaque + ", arme=" + arme + "]";
+        return "Gangster [vie=" + vie + ", attaque=" + attaque + ", defense=" + defense + ", chance=" + chance + ", arme=" + arme + "]";
     }
 
 
