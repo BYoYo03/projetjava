afaf96b1cebb24cadb3fefd5732bcb3a56db1e09
Bahir <bahirboudouma2003@gmail.com>
2024-12-08 15:26:52 +0100

    Noms et prenoms

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,7 +5,7 @@ import com.example.definition.Jeu;
 public class App {
     public static void main(String[] args) {
         System.out.println("");
-        System.out.println("Bienvenue dans le jeu de combat de bahir et merwane");
+        System.out.println("Bienvenue dans le jeu de combat de BOUDOUMA LAMBARKI Bahir et GOUTTAYA Merwane !");
         System.out.println("");
         // Création d'un objet jeu
         Jeu jeu = new Jeu();
